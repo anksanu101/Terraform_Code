dd5ff2548bc5c66d8866a86a3e9764fd5b69e2f2
akash <anksonu92@gmail.com>
2022-01-18 01:27:40 +0530

    Running code

---
+++ PROJECT_MAIN/Testing_DD_FINAL/password.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "e9737076-d38e-4e5a-9a45-e211fc9d59b1"
+  client_id       = "a0b418e1-c3ec-4838-b4be-5d3254ffe746"
+  client_secret   = "voTw93EYBqXIn3sNmLikR7.c3d_1Vgi5Oq"
+  tenant_id       = "f92e1518-ddb2-4504-a71f-45c4be15585f"
+}
+
+
+
